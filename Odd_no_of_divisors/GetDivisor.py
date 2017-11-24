0bb84341b19997b9fe0883da176a6c34d33b488e
Jayakrishna Narra <narrajayakrishna@outlook.com>
2017-11-23 22:09:04 -0500

    added sorted class and timeit module

--- Odd_no_of_divisors/GetDivisor.py
+++ Odd_no_of_divisors/GetDivisor.py
@@ -1,34 +1,37 @@
 import math 
-
 # Calculates the divisor by checking remainder using all natural numbers upto that number (mod)
 # 1 number -> 2 divisors  (divisor1,divisor2) => pair 
 # calculate the least number by mod with divisor1 and then get other, divisor2 = num/divisor1
-
 # *************** Write test cases, BETTER ON NOTEBOOK ON GITHUB , FOLLOW STEPS BY CTCI ********************
-
- 
-#class square_root_sorted(object):
-#    """loop upto square root of number and sort"""
-#    def printdivisors(n):
-
+class square_root_sorted(object):
+    """loop upto square root of number and sort"""
+    def printDivisors(n) :
+        v = []
+        sq_no = int(math.sqrt(n))                   # *** dont add + 1 here , else some divisors repeat
+        for i in range(1,sq_no+1):                   # try to keep loop simple as posible #WHY??Error was sqrt(n) was float hence replaced with new variable
+            if ( n % i ==0):                         # If divisors are equal, print only one
+                if( (n/i) ==  i ):
+                    print (" {} ".format(i), end="")
+                else:                                    
+                    print(" {}".format(i),end ="")    # Print the small divisor 
+                    v.append(int(n/i))                # Push big divisor into array , Note this will be in reverse order
+        for num in reversed(v):                       #reversing a string
+            print (" {}".format(num), end="")
+            #print("{}".format(v[::-1]))              # Print the big divisors in sorted order
 class square_root_unsorted(object):
     """Loop upto square of the number """
     def printDivisors(n) :
-        # Note that this loop runs till square root
-        i = 1
-        sq_no = int(math.sqrt(n))   
-        for i in range(1,sq_no):                       # try to keep loop simple as posible #WHY??Error was sqrt(n) was float hence replaced with new variable
-            if ( n % i ==0):                            
-                                                       # If divisors are equal, print only one
+        sq_no = int(math.sqrt(n))                      # Note that this loop runs till square root
+        for i in range(1,sq_no+1):                     # try to keep loop simple as posible WHY??Error was sqrt(n) was float hence replaced with new variable
+            if ( n % i ==0):                           # If divisors are equal, print only one
                 if( (n/i) ==  i ):
                     print (" {} ".format(i), end="")
-                else:
-                                                       # Otherwise print both (i,n/i)
-                    print(" {} {} ".format(i, int(n/i)),end ="")
-                           
+                else:                                    
+                    print(" {} {} ".format(i, int(n/i)),end ="") # Otherwise print both (i,n/i)
 class natural(object):
     """Loop upto the actual number"""
     def printDivisors(n):
-        for i in range(1,n):
+        for i in range(1,n+1):                          # ******** Remember to add +1 in python range **************
             if(n % i  == 0):
-                print (" {} ".format(i),end="")
+                print (" {}".format(i),end="")
+#end
--- Odd_no_of_divisors/Odd_no_of_divisors.py
+++ Odd_no_of_divisors/Odd_no_of_divisors.py
@@ -4,22 +4,28 @@ import timeit
 
 from GetDivisor import square_root_unsorted as sqno   # once this is done only can use sqno NOT square_root_unsorted
 from GetDivisor import natural as no
-
+from GetDivisor import square_root_sorted as sqno_S
 
 print ("The divisors of 100 are: ")
 
 print("\n\n With O(n)") 
-no.printDivisors(100)
-print("{}".format(timeit.timeit("no.printDivisors(100)", number=100000)))
-
+no.printDivisors(182)
 
 print("\n\n With O(sqtr(n)) but unsorted") 
-sqno.printDivisors(100)
-print(timeit.timeit("no.printDivisors(100)", number=100000))
+sqno.printDivisors(182)
 
-print("\n\n With O(sqtr(n)) and sorted") 
-no.printDivisors(100)
+print("\n\n With O(sqtr(n)) time complexity and O(sqrt(n)) for space and sorted") 
+sqno_S.printDivisors(182)
+
 print("\n")
-print(timeit.timeit("no.printDivisors(100)", number=100000))
 
-timeit.
+###ALways remove printf functions when you want to test the time...
+#SETUP_CODE = '''
+#from GetDivisor import natural as no
+#'''
+#TEST_CODE ='''    
+#no.printDivisors(100)
+#'''
+#print("\n")
+#times = timeit.repeat( stmt = TEST_CODE, setup = SETUP_CODE, repeat = 3, number = 10)
+#print('Naturally executing the divisors {}'.format(min(times)))
